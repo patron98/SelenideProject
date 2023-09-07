e21d3c7f8368346879078c667c0accb0e9a79847
Patron98 <jim.schyvens@gmail.com>
2023-09-07 13:03:52 +0200

    workflow

--- src/test/java/com/patron/TestRunner.java
+++ src/test/java/com/patron/TestRunner.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
-        glue = "com.patron.stepDefinitions",
-        plugin = "json:target/reports.json")
-public class AppTest {
+    glue = "com.patron.stepDefinitions",
+    plugin = "json:target/reports.json")
+public class TestRunner {
 
 }
